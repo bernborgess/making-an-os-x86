39a62f50a4191550e1f8a27ecaf0387ef3e07bc2
bernborgess <bernborgess@outlook.com>
2023-12-30 16:26:41 +0000

    Iterating ABC

--- C2/boot.asm
+++ C2/boot.asm
@@ -1,7 +1,16 @@
 ; Printing a char
 mov ah, 0x0e ; switch to teletype mode
-mov al, 'H'
+mov al, 65
+
+; Iterate the alphabet
 int 0x10 ; BIOS interrupt
+
+; TODO - Alternating CaPs
+loop:
+    inc al
+    int 0x10 ; BIOS interrupt
+    cmp al, 90
+    jl loop
 
 jmp $
 times 510-($-$$) db 0
